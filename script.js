8e445fcacdffe3497b5429edfa6fc4b408f171e2
IM-Rafax <rafal.p4wlowski@gmail.com>
2025-03-01 20:08:58 +0100

    zmiany

--- script.js
+++ script.js
@@ -1,15 +1,20 @@
-let images = document.querySelectorAll("#main img");
+const menuToggle = document.querySelector(".menu-toggle");
+const navLinks = document.querySelector(".nav-links");
+
+menuToggle.addEventListener("click", function () {
+    menuToggle.classList.toggle("active"); // Animacja "X"
+    navLinks.classList.toggle("active");  // Wysuwanie menu
+});
+
+
+let images = document.querySelectorAll(".slider img");
 let dots = document.querySelectorAll(".dot");
 let index = 0;
 let interval;
 
-
-images[index].classList.add("active");
-dots[index].classList.add("active");
-
 function changeImage(newIndex = null) {
     if (newIndex !== null) {
-        index = newIndex; 
+        index = newIndex;
     }
 
     images.forEach(img => img.classList.remove("active"));
@@ -21,19 +26,18 @@ function changeImage(newIndex = null) {
     index = (index + 1) % images.length;
 }
 
-
 function startSlideshow() {
     interval = setInterval(changeImage, 5000);
 }
 
-
 dots.forEach((dot, i) => {
     dot.addEventListener("click", () => {
-        clearInterval(interval); 
-        changeImage(i); 
-        startSlideshow(); 
+        clearInterval(interval);
+        changeImage(i);
+        startSlideshow();
     });
 });
 
-
+images[0].classList.add("active");
+dots[0].classList.add("active");
 startSlideshow();
